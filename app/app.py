557f0a5eb083a3f853e18e66acb6325411d107a5
adelijk <adilkuojji2002@gmail.com>
2025-01-02 01:22:40 +0100

    update flask app

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from kuberentes !!"
+    return "Hello from kuberentes - BD2C !!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
